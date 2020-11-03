e526ea6208ea81bca4749c31a62c291a66d959f5
Willian Wandersee Junior <willianwandersee06@icloud.com>
2020-11-03 16:39:21 -0200

    movimentar cacto

--- dino/js/jogo.js
+++ dino/js/jogo.js
@@ -1,37 +1,61 @@
-console.log("inicio de jogo!");
+console.log("Início do jogo!");
+
 const dino = document.querySelector(".dino");
-//console.log/(dino) ;
+
 let dinoPosition = 0;
 let estaPulando = false;
 
-//verificar se a tecla espaço foi presionada
-document.addEventListener('keydown',(event)=>{
-    if (event.code === 'Space')
-    if(!estaPulando) pular();
+// Verificar se a tecla espaço foi pressionada
+document.addEventListener("keydown", event => {
+  if (event.code === "Space") if (!estaPulando) pular();
 });
 
-function pular(){
-    let intervaloPulo = setInterval(()=>{
-        estaPulando = true;
-        if (dinoPosition >=250){
-           clearInterval(intervaloPulo);
-            let intervaloQueda = setInterval(()=>{
-            if (dinoPosition <= 0 ) {
-                estaPulando = false;
-                clearInterval(intervaloQueda);
-
-            } else {
-                //Decrementar a posição do Dino
-                dinoPosition -= 20;
-                //Atualizar a posição na tela
-                dino.style.bottom = dinoPosition + 'px';
-            }
-            },20);
-
-        }else{
-       //incrementar a posição do Dino
-    dinoPosition += 20;
-    dino.style.bottom = dinoPosition + 'px';
+function pular() {
+  let intervaloPulo = setInterval(() => {
+    estaPulando = true;
+    if (dinoPosition >= 250) {
+      clearInterval(intervaloPulo);
+      let intervaloQueda = setInterval(() => {
+        if (dinoPosition <= 0) {
+          estaPulando = false;
+          clearInterval(intervaloQueda);
+        } else {
+          // Decrementar a posição do Dino
+          dinoPosition -= 20;
+          // Atualizar a posição na tela
+          dino.style.bottom = dinoPosition + "px";
         }
-},20);
-}  
+      }, 20);
+    } else {
+      // Incrementar a posilção do Dino
+      dinoPosition += 20;
+      // Atualizar a posição na tela
+      dino.style.bottom = dinoPosition + "px";
+    }
+  }, 20);
+}
+
+// Criação de Cactos
+const background = document.querySelector(".background");
+function criarCacto() {
+  let cactoPosition = 1000;
+  const cacto = document.createElement("div");
+  // Tempo aleatório entre 1s e 6s
+  let tempoRandom = Math.random() * 5000 + 100;
+  cacto.classList.add("cacto");
+  cacto.style.left = 1000 + "px";
+  background.appendChild(cacto);
+
+  let intervaloEsquerda = setInterval(() => {
+    if (cactoPosition <= -60) {
+    } else {
+      cactoPosition -= 10;
+      cacto.style.left = cactoPosition + "px";
+    }
+  }, 55);
+  let tempoCacto = setTimeout(criarCacto, tempoRandom);
+      
+  
+}
+
+criarCacto();
